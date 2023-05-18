b7d03752daddd6cf2b806ffdb5e78a2402d068a0
salespanadol <133994474+salespanadol@users.noreply.github.com>
2023-05-18 23:39:32 +0700

    Add files via upload

---
+++ tugas1/tugas1.js
@@ -0,0 +1,20 @@
+
+for (let i = 1; i <= 10; i++)
+{
+    console.log(i); 
+}
+
+console.log();
+
+for (let i = 1; i <= 10; i++)
+{
+    if (i % 2 === 0)
+    {
+        console.log(i + " Bilangan Genap");
+    }
+
+    else if (i % 2 === 1)
+    {
+        console.log(i + " Bilangan Ganjil");
+    }
+}
